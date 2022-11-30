496a49531f99127d041f9c8388b3b436a0620e2a
motaouia <med.motaouia@gmail.com>
2022-11-30 14:41:04 +0100

    rest-api-with-dto

--- src/main/java/org/medmota/restdemo/RestApiWithResponseEntityApplication.java
+++ src/main/java/org/medmota/restdemo/RestApiWithResponseEntityApplication.java
@@ -1,13 +1,20 @@
 package org.medmota.restdemo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApiWithResponseEntityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiWithResponseEntityApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/org/medmota/restdemo/dto/UserDTO.java
@@ -0,0 +1,64 @@
+package org.medmota.restdemo.dto;
+
+public class UserDTO {
+
+	private Integer id;
+	private String userName;
+	private String mobileNo;
+	private String emailId;
+	private String city;
+	private String password;
+
+	public UserDTO() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
